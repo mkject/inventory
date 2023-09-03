34f6b9851f7d0bc587d9daa956d2a1cab8fdf390
unknown <mknetprojects@gmail.com>
2023-09-03 20:48:06 +0700

    edit settings.py

--- core/settings.py
+++ core/settings.py
@@ -145,8 +145,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
