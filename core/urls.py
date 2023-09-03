dcd24ceea1b335d553524c0b2dee13fa3fb9eab1
unknown <mknetprojects@gmail.com>
2023-09-03 20:47:08 +0700

    edit urls.py

--- core/urls.py
+++ core/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('inventory/', include('inventory.urls')),
     path('transactions/', include('transactions.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
